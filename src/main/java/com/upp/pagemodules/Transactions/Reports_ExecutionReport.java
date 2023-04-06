08c79500611e4dc5fc58c4458b52f53dad99b55e
Karthik T <karthik.t@appveen.com>
2023-04-06 11:52:03 +0530

    Updated eCommExecutionsReport

--- src/main/java/com/upp/pagemodules/Transactions/Reports_ExecutionReport.java
+++ src/main/java/com/upp/pagemodules/Transactions/Reports_ExecutionReport.java
@@ -29,6 +29,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import callbackInterfaces.ICallback;
+
 public class Reports_ExecutionReport extends BaseClass {
 
 	public static Object_NewDeal od;
@@ -44,7 +45,8 @@ public class Reports_ExecutionReport extends BaseClass {
 	public static DateUtils dateutil;
 	public static ScrollTypes scroll;
 	public static String productName;
-	 public static Object_Transactions tm ;
+	public static Object_Transactions tm;
+
 	public Reports_ExecutionReport() {
 
 		od = new Object_NewDeal();
@@ -55,93 +57,83 @@ public class Reports_ExecutionReport extends BaseClass {
 		jsClick = new JavascriptClick(driver);
 		scroll = new ScrollTypes(driver);
 		dateutil = new DateUtils();
-		tm=new Object_Transactions();
+		tm = new Object_Transactions();
 
 	}
-	
-	public void ExecutionReport(String TSID,String DealId) throws Exception
-	{
-		
-		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsIcon,Duration.ofSeconds(15));
+
+	public void ExecutionReport(String TSID, String DealId) throws Exception {
+
+		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsIcon, Duration.ofSeconds(15));
 		jsClick.click(tm.reports_ReportsIcon);
-		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsInternal,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsInternal, Duration.ofSeconds(5));
 		jsClick.click(tm.reports_ReportsInternal);
-		applyExplicitWaitsUntilElementClickable(tm.reports_searchBox,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_searchBox, Duration.ofSeconds(5));
 		tm.reports_searchBox.sendKeys("Execution Report");
-		applyExplicitWaitsUntilElementClickable(tm.reports_ExecutionReport,Duration.ofSeconds(6));
+		applyExplicitWaitsUntilElementClickable(tm.reports_ExecutionReport, Duration.ofSeconds(6));
 		jsClick.click(tm.reports_ExecutionReport);
-		applyExplicitWaitsUntilElementClickable(tm.reports_DealId,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_DealId, Duration.ofSeconds(5));
 		tm.reports_DealId.sendKeys(DealId);
-		applyExplicitWaitsUntilElementClickable(tm.reports_SubmitButton,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_SubmitButton, Duration.ofSeconds(5));
 		jsClick.click(tm.reports_SubmitButton);
 		Thread.sleep(4000);
-		String ScroeStatus=tm.reports_ScroeStatus.getText();
-		System.out.println("Scroe status is "+ScroeStatus);
-		if(ScroeStatus.equalsIgnoreCase("Pending") || ScroeStatus.equalsIgnoreCase("Scheduled"))
-		{
+		String ScroeStatus = tm.reports_ScroeStatus.getText();
+		System.out.println("Scroe status is " + ScroeStatus);
+		if (ScroeStatus.equalsIgnoreCase("Pending") || ScroeStatus.equalsIgnoreCase("Scheduled")) {
 			System.out.println("Waiting for Transaction to be triggered");
 			TimeUnit.SECONDS.sleep(5);
-			String ScroeStatusafter=tm.reports_ScroeStatus.getText();
-			if(ScroeStatusafter.equalsIgnoreCase("triggered"))
-			{
+			String ScroeStatusafter = tm.reports_ScroeStatus.getText();
+			if (ScroeStatusafter.equalsIgnoreCase("triggered")) {
 				System.out.println("Transaction is triggered");
-				
-			}
-			else
-			{
+
+			} else {
 				System.out.println("Transaction is not yet triggered");
 			}
 		}
-		
+
 	}
-	
-	
-	public void eCommExecutionsReport(String EndToEndId,String DealId) throws Exception
-	{
-		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsIcon,Duration.ofSeconds(15));
+
+	public void eCommExecutionsReport(String EndToEndId, String DealId) throws Exception {
+		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsIcon, Duration.ofSeconds(15));
 		jsClick.click(tm.reports_ReportsIcon);
-		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsInternal,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_ReportsInternal, Duration.ofSeconds(5));
 		jsClick.click(tm.reports_ReportsInternal);
-		applyExplicitWaitsUntilElementClickable(tm.reports_searchBox,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_searchBox, Duration.ofSeconds(5));
 		tm.reports_searchBox.sendKeys("eComm Executions");
-		applyExplicitWaitsUntilElementClickable(tm.reports_eCommExecutions,Duration.ofSeconds(6));
+		applyExplicitWaitsUntilElementClickable(tm.reports_eCommExecutions, Duration.ofSeconds(6));
 		jsClick.click(tm.reports_eCommExecutions);
-		applyExplicitWaitsUntilElementClickable(tm.reports_endToendId,Duration.ofSeconds(5));
+		applyExplicitWaitsUntilElementClickable(tm.reports_endToendId, Duration.ofSeconds(5));
 		tm.reports_endToendId.sendKeys(EndToEndId);
-		
-		scroll.scrollHorizontalInsideWindow(tm.reports_horizontalWindow,1800);
-		
+
+		scroll.scrollHorizontalInsideWindow(tm.reports_horizontalWindow, 1800);
+
 		Thread.sleep(3000);
-		
+
 		By Amount1 = By.xpath("//div[normalize-space()='925']");
-		applyExplicitWaitsUntilElementVisible(Amount1,3);
-		String amount1=driver.findElement(Amount1).getText();
-		System.out.println("the amount1 is:"+amount1);
-	
+		applyExplicitWaitsUntilElementVisible(Amount1, 3);
+		String amount1 = driver.findElement(Amount1).getText();
+		System.out.println("the amount1 is:" + amount1);
+
 		By Amount2 = By.xpath("//div[normalize-space()='75']");
-		applyExplicitWaitsUntilElementVisible(Amount2,3);
-		String amount2=driver.findElement(Amount2).getText();
-		System.out.println("the amount2 is:"+amount2);
-		
-		By Status=By.xpath("//div[normalize-space()='Scheduled']");
-		applyExplicitWaitsUntilElementVisible(Status,3);
-		String status=driver.findElement(Status).getText();
-		System.out.println("the status is:"+status);
-		
-		if(!(status.equalsIgnoreCase("Scheduled")))
-		{
+		applyExplicitWaitsUntilElementVisible(Amount2, 3);
+		String amount2 = driver.findElement(Amount2).getText();
+		System.out.println("the amount2 is:" + amount2);
+
+		By Status = By.xpath("//div[normalize-space()='Scheduled']");
+		applyExplicitWaitsUntilElementVisible(Status, 3);
+		String status = driver.findElement(Status).getText();
+		System.out.println("the status is:" + status);
+
+		if (!(status.equalsIgnoreCase("Scheduled"))) {
 			Assert.fail("Transaction is not scheduled");
 		}
-		
-		if(!(amount1.equalsIgnoreCase("925")))
-		{
+
+		if (!(amount1.equalsIgnoreCase("925"))) {
 			Assert.fail("Amount is mismatched");
 		}
-		
-		if(!(amount2.equalsIgnoreCase("75")))
-		{
+
+		if (!(amount2.equalsIgnoreCase("75"))) {
 			Assert.fail("Amount is mismatched");
 		}
 	}
-	
+
 }
