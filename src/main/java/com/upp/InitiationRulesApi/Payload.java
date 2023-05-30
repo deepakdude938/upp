13b2b2fa1fece31ed281d4609d0ff1c2c6c6e058
Karthik T <karthik.t@appveen.com>
2023-05-30 12:21:29 +0530

    Added TS36

--- src/main/java/com/upp/InitiationRulesApi/Payload.java
+++ src/main/java/com/upp/InitiationRulesApi/Payload.java
@@ -104,5 +104,30 @@ public class Payload extends BaseClass{
 		return modifiedJsonString;
 
 	}
+	
+	public static String Rule_OBOParticipant_OBO_Info_Not_Null(String dealId, String TSID) throws IOException, Exception {
+		externalData = new ExcelReader();
+		String payLoadString = externalData.getFieldData(TSID, "Initiation Rules", "Payload");
+
+		long number = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
+		String random = Long.toString(number);
+		String uniquePlatformRefNo = "PlatformRef" + random;
+		
+
+		String utcdate = DateUtils.getCurrentDateUTC();
+
+		String utctimeEod = utcdate + "T" + "14:30:00Z";
+
+		DocumentContext jsonContext = JsonPath.parse(payLoadString);
+		jsonContext.set("$.paymentInfo.platformRefNo", uniquePlatformRefNo);
+		jsonContext.set("$.dealRefId", dealId);
+		jsonContext.set("$.paymentInfo.accountNumber",DealPage.AccountNo1);
+		jsonContext.set("$.creditTransactionInfo[0].requestedExecutionOn",utctimeEod);
+	
+		String modifiedJsonString = jsonContext.jsonString();
+
+		return modifiedJsonString;
+
+	}
 
 }
