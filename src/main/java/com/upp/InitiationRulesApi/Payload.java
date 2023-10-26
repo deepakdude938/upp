e7f0094c1637846882698e8c04a855af4469d490
Karthik T <karthik.t@appveen.com>
2023-10-26 16:55:44 +0530

    Added TS119

--- src/main/java/com/upp/InitiationRulesApi/Payload.java
+++ src/main/java/com/upp/InitiationRulesApi/Payload.java
@@ -686,4 +686,27 @@ public class Payload extends BaseClass {
 
 		return modifiedJsonString;
 	}
+	
+	public static String Rule_With_Account_Key(String dealId, String TSID) throws IOException, Exception {
+		externalData = new ExcelReader();
+		String payLoadString = externalData.getFieldData(TSID, "Initiation Rules", "Payload");
+
+		long number = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
+		String random = Long.toString(number);
+		String uniquePlatformRefNo = "PlatformRef" + random;
+
+		String utcdate = DateUtils.getCurrentDateUTC();
+		String plus2mins=DateUtils.getCurrentTimeUTCPlus2Minutes();
+
+		String utctimeEod = utcdate + "T" + plus2mins;
+
+		DocumentContext jsonContext = JsonPath.parse(payLoadString);
+		jsonContext.set("$.paymentInfo.platformRefNo", uniquePlatformRefNo);
+		jsonContext.set("$.creditTransactionInfo[0].requestedExecutionOn", utctimeEod);
+		jsonContext.set("$.dealRefId", dealId);
+		String modifiedJsonString = jsonContext.jsonString();
+
+		return modifiedJsonString;
+
+	}
 }
