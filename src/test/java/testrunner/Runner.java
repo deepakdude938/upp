26016ca070ba52bebc4c11c11a6c5d2d9e238976
manisha <manisha_vtest@appveen.com>
2024-01-08 18:35:50 +0530

    Update Runner.java

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="./testcases/TS110.feature",
+@CucumberOptions(features="./testcases/",
 
 //				dryRun = true,
 				glue={"com.upp.stepdefinition","com.upp.hooks"},
